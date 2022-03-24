a3992d19918b6a00ab0fc19dc7b2b8bfcdd27b0f
dirkilia <86527488+dirkilia@users.noreply.github.com>
2022-03-24 17:46:34 +0300

    directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'mapnote';
+
+  attributes: {id:string} = {
+    id: 'paragraph1'
+  }
+
+  onClick():void {
+    this.attributes = this.attributes.id === 'paragraph1' ? {id: 'paragraph2'} : {id: 'paragraph1'}
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AttributeDirective } from './attribute.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttributeDirective
   ],
   imports: [
     BrowserModule,
