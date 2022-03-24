f62ad3c3d599421e61fcb418048a75a409bf0294
dirkilia <86527488+dirkilia@users.noreply.github.com>
2022-03-25 02:56:40 +0300

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
 
-interface CardInfo {
-  name: string,
-  number: string,
-  amount: string,
-  expirationDate: string,
-  paymentSystem: "visa" | "mastercard"
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,12 +8,5 @@ interface CardInfo {
 
 
 export class AppComponent {
-  title: string = 'mapnote';
-  card_info: CardInfo = {
-    name: "some card name",
-    number: "0000000000000000",
-    amount: "4989,33₽",
-    expirationDate: "02/33",
-    paymentSystem: "visa"
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
+import { FormatNumberPipe } from './format-number.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
+    FormatNumberPipe,
   ],
   imports: [
     BrowserModule,
